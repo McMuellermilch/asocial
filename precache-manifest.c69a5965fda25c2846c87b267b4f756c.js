30309f18bb691b419dca7da3b078e4584990bf85
Florian <mueller.florian99@outlook.com>
2020-10-03 15:51:44 +0200

    Updates

---
+++ precache-manifest.c69a5965fda25c2846c87b267b4f756c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed53c6fb05d66894ecb02af5f5cd743",
+    "url": "/asocial/index.html"
+  },
+  {
+    "revision": "ea0739e30ea23937fd51",
+    "url": "/asocial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90643eca54d3e865d774",
+    "url": "/asocial/static/js/2.e7115349.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/asocial/static/js/2.e7115349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0739e30ea23937fd51",
+    "url": "/asocial/static/js/main.b6583a12.chunk.js"
+  },
+  {
+    "revision": "4e627b206780879bb887",
+    "url": "/asocial/static/js/runtime-main.98c022f8.js"
+  }
+]);
